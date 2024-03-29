45a77cf0ce6075ef1b85bc8a133114601e10249a
Tomás Gómez Molina <tomas@conomyhq.com>
2024-03-29 10:51:41 -0300

    add(scheme)

--- src/entities/message.ts
+++ src/entities/message.ts
@@ -1,38 +1,14 @@
 
-class Message {
+class MessageSchema {
     id: string;
-    TSN: string;
-    OSN: string;
-    NSE: string;
     messageCode: string;
-    destination: string;
     description: string;
-    priority: string;
-    status: string;
-    sender: string;
-    creationDate: string;
-    creationTime: string;
-    receiver: string;
-    receivedDate: string;
-    receivedTime: string;
     parameters: Parameter[];
-    
-        constructor(id: string, TSN: string, OSN: string, NSE: string, messageCode: string, destination: string, description: string, priority: string, status: string, sender: string, creationDate: string, creationTime: string, receiver: string, receivedDate: string, receivedTime: string, parameters: Parameter[]) {
-            this.id = id;
-            this.TSN = TSN;
-            this.OSN = OSN;
-            this.NSE = NSE;
-            this.messageCode = messageCode;
-            this.destination = destination;
-            this.description = description;
-            this.priority = priority;
-            this.status = status;
-            this.sender = sender;
-            this.creationDate = creationDate;
-            this.creationTime = creationTime;
-            this.receiver = receiver;
-            this.receivedDate = receivedDate;
-            this.receivedTime = receivedTime;
-            this.parameters = parameters;
-        }
+
+    constructor(id: string, messageCode: string, description: string, parameters: Parameter[]) {
+        this.id = id;
+        this.messageCode = messageCode;
+        this.description = description;
+        this.parameters = parameters;
+    }
 }
