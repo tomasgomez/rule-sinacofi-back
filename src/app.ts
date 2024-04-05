a46a7e40eb8b56a60e9f4bf9790084fa29056111
Tomás Gómez Molina <tomas@conomyhq.com>
2024-04-05 16:51:17 -0300

    feat(api): get api mock for types and schemas

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import express from 'express';
+import router from './adapters/express/routes'
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, Express with TypeScript!  ');
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server esta running on http://localhost:${port}`);
 });
--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,17 +1,17 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-export const processError = (error: Error, message: string | undefined = undefined) => {
-    if (error.name && error.name === "TokenExpiredError") {
-        return NextResponse.json({ "message": "token expired" }, {
-            status: 401,
-        });
-    }
-    if (error.name && error.name == "PermissionError") {
-        return NextResponse.json({ "message": error.message }, {
-            status: 403,
-        });
-    }
-    return NextResponse.json({ message: message || error.toString() }, {
-        status: 500,
-    });
-}
+// export const processError = (error: Error, message: string | undefined = undefined) => {
+//     if (error.name && error.name === "TokenExpiredError") {
+//         return NextResponse.json({ "message": "token expired" }, {
+//             status: 401,
+//         });
+//     }
+//     if (error.name && error.name == "PermissionError") {
+//         return NextResponse.json({ "message": error.message }, {
+//             status: 403,
+//         });
+//     }
+//     return NextResponse.json({ message: message || error.toString() }, {
+//         status: 500,
+//     });
+// }
