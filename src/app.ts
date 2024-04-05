4f5aade60b1dc90ec186fa287839585406da0a7f
Tomás Gómez Molina <tomas@conomyhq.com>
2024-04-05 16:51:53 -0300

    fix(port)

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import router from './adapters/express/routes'
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.json());
 app.use(router);
