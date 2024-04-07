156bbef4ee9680df84b2b78d0b89f1025841e3c1
Tomás Gómez Molina <tomas@conomyhq.com>
2024-04-07 11:53:53 -0400

    add(app)

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,17 @@ import router from './adapters/express/routes'
 const app = express();
 const port = 3001;
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(express.json());
 app.use(router);
+
+
 
 app.listen(port, () => {
   console.log(`Server esta running on http://localhost:${port}`);
