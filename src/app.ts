b1eac656250181b28df299ddebe9fdbe2e9b0534
Tomás Gómez Molina <tomas@conomyhq.com>
2024-04-07 11:58:06 -0400

    remove merge

--- src/app.ts
+++ src/app.ts
@@ -38,19 +38,3 @@ server.setLogger(logResponse);
 // start server
 server.start(3000);
 
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-
-app.use(express.json());
-app.use(router);
-
-
-
-app.listen(port, () => {
-  console.log(`Server esta running on http://localhost:${port}`);
-});
