f5304e9316ec1b48a03c46a8893da409be7ca46a
felipegarciacontreras <felipe.garcia@coddde.com>
2024-05-03 16:55:20 -0400

    query

--- src/repository/schema/methods/findSchema.ts
+++ src/repository/schema/methods/findSchema.ts
@@ -14,9 +14,66 @@ const findSchema = async(context: ISchemaRepository, schema: IOptionalSchema, se
         
         /* Initialize the where object with the possible attributes to search with if schema is empty return all schemas */
         const where = { messageCode: schema.messageCode }
-
+        const select = {
+            id: true,
+            messageCode: true,
+            description: true,
+            name: true,
+            parameters: {
+                select: {
+                    name: true,
+                    messageCode: true, 
+                    label: true, 
+                    type: true, 
+                    placeholder: true, 
+                    priority: true,
+                    row: true, 
+                    column: true, 
+                    defaultValue: true,
+                    rules: {
+                        select: {
+                            rule: {
+                                select: {
+                                    name: true,
+                                    condition: true,
+                                    type: true,
+                                    value: true,
+                                }
+                            }
+                        }
+                    },
+                    optionValues: {
+                        select: {
+                            optionValue: {
+                                select: {
+                                    value: true,
+                                    label: true,
+                                }
+                             }
+                        }
+                    },
+                },
+            } 
+        };
+        const schemaFetched = await client.schema.findUnique({ where, select });
     
-        const schemaFetched: any = await client.schema.findUnique({ where: where, include: { parameters: { include: {optionValues: true, rules: true }} } });
+        // const schemaFetched = await client.schema.findFirst({ where }).parameters(
+        //     {
+        //         include: {
+        //             rules: {
+        //                 include: {
+        //                     rule: true
+        //                 }
+        //             },
+        //             optionValues: {
+        //                 include: {
+        //                     optionValue: true
+        //                 }
+        //         }
+        //     }
+        // }
+        // );
+        
         
         /* If the schema is not found, return an error */
         if (!schemaFetched) {
@@ -27,16 +84,16 @@ const findSchema = async(context: ISchemaRepository, schema: IOptionalSchema, se
         // parameters
         const parametersAdapted: any[] = schemaFetched.parameters.map((parameter:any) => {
             const { name, schemaName, label, type, placeholder, description, priority, rules, row, column, defaultValue, optionValues } = parameter;
-                const rulesAdapted = rules.map((rule: any) => new Rule(rule.id, rule.name,rule.type,rule.description,rule.condition,rule.value,rule.priority,rule.createdAt,rule.updatedAt));
+                const rulesAdapted = rules.map((rule: any) => new Rule(rule.rule.id, rule.rule.name,rule.rule.type,rule.rule.description,rule.rule.condition,rule.rule.value,rule.rule.priority,rule.rule.createdAt,rule.rule.updatedAt));
                 const optionValuesAdapted = optionValues.map((optionValue: any) => {
-                const opt: IOptionValue  = {
-                    name: optionValue.name,
-                    type: optionValue.type,
-                    description: optionValue.description,
-                    value: optionValue.value,
-                    label: optionValue.label,
-                    createdAt: optionValue.createdAt,
-                    updatedAt: optionValue.updatedAt   
+                    const opt: IOptionValue  = {
+                    name: optionValue.optionValue.name,
+                    type: optionValue.optionValue.type,
+                    description: optionValue.optionValue.description,
+                    value: optionValue.optionValue.value,
+                    label: optionValue.optionValue.label,
+                    createdAt: optionValue.optionValue.createdAt,
+                    updatedAt: optionValue.optionValue.updatedAt   
                 }
                 return opt;
             })
@@ -45,8 +102,7 @@ const findSchema = async(context: ISchemaRepository, schema: IOptionalSchema, se
         })
 
 
-        
-        return new Schema(schemaFetched.id, schemaFetched.messageCode, schemaFetched.description, schemaFetched.name, schemaFetched.createdAt, schemaFetched.updatedAt, parametersAdapted, []);
+        return new Schema(schemaFetched.id, schemaFetched.messageCode, schemaFetched.description, schemaFetched.name, new Date(), new Date(), parametersAdapted, []);
 
 
     } catch (error: any) {
